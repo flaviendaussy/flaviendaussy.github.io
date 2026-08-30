67f7babca46ff3b5a5da7c053a3dd881167693c2
Flavien Daussy <flavien.daussy@gmail.com>
2026-08-30 19:23:47 +0100

    fix(camera,scan): default to back camera, support lens flip and cross-platform web image scan

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "505988087" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1306667830" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
