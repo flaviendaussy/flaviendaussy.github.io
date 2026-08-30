49d4b44ff2abdd0a506009d6e2a49db285521c92
Flavien Daussy <flavien.daussy@gmail.com>
2026-08-30 19:01:13 +0100

    deploy: update web build with GIS map engine and interactive terroir resolver

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "893880018" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1411686409" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
